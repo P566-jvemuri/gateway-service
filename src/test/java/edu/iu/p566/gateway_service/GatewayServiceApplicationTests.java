8eb4cac965446cfe1864176ecbcf2a0ac802066e
jahnavi-vemuri <jvemuri@iu.edu>
2025-03-06 16:57:42 -0500

    practicum5

---
+++ src/test/java/edu/iu/p566/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.p566.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
